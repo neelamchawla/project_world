de029253ff2cede249a4bac2e34467dfe224e91c
neelamchawla <neelamchawla55@gmail.com>
2020-04-10 22:16:26 +0530

    Updates

---
+++ precache-manifest.f7b22323a9a5f77c30c76e0bb137d0f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb34c110b2e579060086cdcdfa9325b",
+    "url": "/neelamchawla/project_world.git/index.html"
+  },
+  {
+    "revision": "cf6a12541bfbe9e13643",
+    "url": "/neelamchawla/project_world.git/static/css/2.e5a96deb.chunk.css"
+  },
+  {
+    "revision": "635ed9029f235bd8e859",
+    "url": "/neelamchawla/project_world.git/static/css/main.f3e3804c.chunk.css"
+  },
+  {
+    "revision": "cf6a12541bfbe9e13643",
+    "url": "/neelamchawla/project_world.git/static/js/2.bf1047c0.chunk.js"
+  },
+  {
+    "revision": "f89c9b0960761b2023f3041c41c07842",
+    "url": "/neelamchawla/project_world.git/static/js/2.bf1047c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635ed9029f235bd8e859",
+    "url": "/neelamchawla/project_world.git/static/js/main.9fb321a8.chunk.js"
+  },
+  {
+    "revision": "81ce028b62d33db5db8c",
+    "url": "/neelamchawla/project_world.git/static/js/runtime-main.940425d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neelamchawla/project_world.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
